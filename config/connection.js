2666361fd7a4b9401d0fd0a831b6980bb9f70bd1
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2021-09-04 12:00:37 -0400

    code update

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// Create connection to our database, pass in your MySQL information for username and password
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize('tech-blog','root', 'LEEHIGHWAY22046@#$', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
+module.exports = sequelize;
